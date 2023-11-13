1d23381a8e3b868eb406a2e36af239e96ecce2d0
BwithE <144924113+BwithE@users.noreply.github.com>
2023-11-12 16:22:37 -0800

    Update ello-stack.sh

--- ello-stack.sh
+++ ello-stack.sh
@@ -101,7 +101,8 @@ if [ -e /etc/filebeat/filebeat.yml  ]; then
 # export kibana index-patterns
 #curl -X POST "127.0.0.1:5601/api/saved_objects/_export" -H "kbn-xsrf: true" -H "Content-Type: application/json" --data '{"objects":[{"type":"index-pattern","id":"local-nmap"}]}' --output local-nmap.ndjson
 # import kibana index-patterns
-curl -X POST "127.0.0.1:5601/api/saved_objects/_import" -H "kbn-xsrf: true" -H "Content-Type: multipart/form-data" -F file=@Index-Patterns/nmap.ndjson
+# can't be imorted until there is data, so the nmap report has to be built first
+#curl -X POST "127.0.0.1:5601/api/saved_objects/_import" -H "kbn-xsrf: true" -H "Content-Type: multipart/form-data" -F file=@Index-Patterns/nmap.ndjson
 
     
 else
