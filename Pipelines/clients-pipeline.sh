b34e2d880fca41f9c0e8a1770b17c8f94ca9ab22
BwithE <144924113+BwithE@users.noreply.github.com>
2023-11-23 15:21:09 -0800

    Update clients-pipeline.sh

--- Pipelines/clients-pipeline.sh
+++ Pipelines/clients-pipeline.sh
@@ -8,23 +8,10 @@ curl -X PUT "127.0.0.1:9200/_ingest/pipeline/local-clients-pipeline" -H 'Content
       "field": "message",
       "target_fields": [
         "Client MAC",
-        "First time seen",
-        "Last time seen",
-        "Power",
-        "# packets",
         "BSSID",
         "ESSID"
       ],
       "ignore_missing": false
-    }
-  },
-  {
-    "date": {
-      "field": "First time seen",
-      "timezone": "America/New_York",
-      "formats": [
-        "yyyy-MM-dd HH:mm:ss"
-      ]
     }
   },
   {
