128553dcb78d6ffeb0a285a583fafb3e4b19f19f
BwithE <144924113+BwithE@users.noreply.github.com>
2023-11-23 15:06:29 -0800

    Update airodump-report.sh

--- Scripts/airodump-report.sh
+++ Scripts/airodump-report.sh
@@ -51,7 +51,16 @@ awk '/^Station MAC,First time seen,Last time seen,Power,# packets,BSSID,Probed E
 sed -i 's/Probed ESSIDs/ESSID/g' $clients
 sed -i 's/Station MAC/Client MAC/g' $clients
 
-mv $aps /home/kali/elk/aps
-mv $clients /home/kali/elk/clients
+grep -v -E '^\s*$' $aps | cut -d "," -f1,4,6,7,8,14 > /home/kali/elk/aps/aps.csv
+grep -v -E '^\s*$' $clients | cut -d "," -f1,6,7 > /home/kali/elk/clients/clients.csv
+
+#cat $aps | cut -d "," -f1,4,6,7,8,14 > /home/kali/elk/aps/aps.csv
+#cat $clients | cut -d "," -f1,6,7 > /home/kali/elk/clients/clients.csv
+
+rm -f $aps 
+rm -f $clients
+
+#mv $aps /home/kali/elk/aps
+#mv $clients /home/kali/elk/clients
 
 rm -f $input
