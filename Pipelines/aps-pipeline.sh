80482feaffb327a66c7b33ff9d73fd71bb88e2e4
BwithE <144924113+BwithE@users.noreply.github.com>
2023-11-23 14:52:04 -0800

    Update aps-pipeline.sh

--- Pipelines/aps-pipeline.sh
+++ Pipelines/aps-pipeline.sh
@@ -8,52 +8,13 @@ curl -X PUT "127.0.0.1:9200/_ingest/pipeline/local-aps-pipeline" -H 'Content-Typ
       "field": "message",
       "target_fields": [
         "BSSID",
-        "First time seen",
-        "Last time seen",
         "channel",
-        "Speed",
         "Privacy",
         "Cipher",
         "Authentication",
-        "Power",
-        "# beacons",
-        "# IV",
-        "LAN IP",
-        "ID-length",
-        "ESSID",
-        "Key"
+        "ESSID"
       ],
       "ignore_missing": false
-    }
-  },
-  {
-    "date": {
-      "field": "First time seen",
-      "timezone": "America/New_York",
-      "formats": [
-        "yyyy-MM-dd HH:mm:ss"
-      ]
-    }
-  },
-  {
-    "convert": {
-      "field": "Power",
-      "type": "long",
-      "ignore_missing": true
-    }
-  },
-  {
-    "convert": {
-      "field": "Speed",
-      "type": "long",
-      "ignore_missing": true
-    }
-  },
-  {
-    "convert": {
-      "field": "channel",
-      "type": "long",
-      "ignore_missing": true
     }
   },
   {
