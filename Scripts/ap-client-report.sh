2505f2b6b19a25b4203743ee8189c9bfde77adc4
BwithE <144924113+BwithE@users.noreply.github.com>
2023-11-23 17:50:54 -0800

    Update ap-client-report.sh

--- Scripts/ap-client-report.sh
+++ Scripts/ap-client-report.sh
@@ -10,7 +10,7 @@ awk '/^Station MAC,First time seen,Last time seen,Power,# packets,BSSID,Probed E
 sed -i 's/Probed ESSIDs/ESSID/g' $clients
 sed -i 's/Station MAC/Client MAC/g' $clients
 
-mv $aps /home/kali/elk/aps
-mv $clients /home/kali/elk/clients
+grep -v -E '^\s*$' $aps | cut -d "," -f1,4,6,7,8,14 > /home/kali/elk/aps/aps.csv
+grep -v -E '^\s*$' $clients | cut -d "," -f1,6,7 > /home/kali/elk/clients/clients.csv
 
 rm -f $input
